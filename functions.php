e0768ab07b00f1940f5202b8d5b454286007ff00
Tantowi JH <71215938+tantowijh@users.noreply.github.com>
2024-01-23 23:20:27 +0700

    update parameter info

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 include 'koneksi.php';
 
-function sqlResult($stmt){
+function sqlResult(mysqli_stmt $stmt){
     if ($stmt->execute()) {
         return $stmt->affected_rows;
     } else {
@@ -9,17 +9,17 @@ function sqlResult($stmt){
     }
 }
 
-function tabelPendidikan($koneksi){
+function tabelPendidikan(mysqli $koneksi){
     $sql = "SHOW TABLES LIKE 'data_pendidikan'";
     return $koneksi->query($sql)->num_rows > 0;
 }
 
-function dataPendidikan($koneksi){
+function dataPendidikan(mysqli $koneksi){
     $sql = "SELECT * FROM data_pendidikan";
     return $koneksi->query($sql);
 }
 
-function ddl_tabel($koneksi){
+function ddl_tabel(mysqli $koneksi){
     $sql = "CREATE TABLE IF NOT EXISTS data_pendidikan (
         id INT(11) AUTO_INCREMENT PRIMARY KEY,
         tingkat VARCHAR(7) NOT NULL,
@@ -33,12 +33,12 @@ function ddl_tabel($koneksi){
     return $koneksi->query($sql);
 }
 
-function dml_select($koneksi, $id){
+function dml_select(mysqli $koneksi, int $id){
     $sql = "SELECT * FROM data_pendidikan WHERE id = $id";
     return $koneksi->query($sql);
 }
 
-function dml_insert($koneksi, $data){
+function dml_insert(mysqli $koneksi, array $data){
     $sql = "INSERT INTO data_pendidikan (tingkat, nama_sekolah, jurusan, tahun_mulai, tahun_selesai, nilai_akhir, bersertifikat)
             VALUES (?, ?, ?, ?, ?, ?, ?)";
     $sql = $koneksi->prepare($sql);
@@ -46,14 +46,14 @@ function dml_insert($koneksi, $data){
     return sqlResult($sql);
 }
 
-function dml_update($koneksi, $data, $id){
+function dml_update(mysqli $koneksi, array $data, int $id){
     $sql = "UPDATE data_pendidikan SET tingkat=?, nama_sekolah=?, jurusan=?, tahun_mulai=?, tahun_selesai=?, nilai_akhir=?, bersertifikat=? WHERE id=?";
     $sql = $koneksi->prepare($sql);
     $sql->bind_param("sssssssi", $data['tingkat'], $data['nama_sekolah'], $data['jurusan'], $data['tahun_mulai'], $data['tahun_selesai'], $data['nilai_akhir'], $data['bersertifikat'], $id);
     return sqlResult($sql);
 }
 
-function dml_delete($koneksi, $id){
+function dml_delete(mysqli $koneksi, int $id){
     $sql = $koneksi->prepare("DELETE FROM data_pendidikan WHERE id=?");
     $sql->bind_param("i", $id);
     return sqlResult($sql);
